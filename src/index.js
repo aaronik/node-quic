a19bf871c50d5acfe7cacc17c4672dedcc9d5c84
Aaron Sullivan <aasullivan1618@gmail.com>
2018-07-18 17:26:01 -0700

    Built out and finished custom promise module

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { Client, Server } from 'quic'
+
 // quic.listen(2345) // also takes address, defaults to localhost
 //   .then(() => {})
 //   .onError(error => {})
@@ -15,13 +17,145 @@
 //   .onError(error => {})
 //   .onData(data => {})
 
+// simple convenience helper
+const rejectPromise = (promise, err, message) => {
+  promise.reject(Object.assign(err, { class: message }))
+}
+
+// A special promise with .onError and .onData functions
+// Also does not require executor function, and can have reject
+// called on its prototype
+class CustomPromise {
+  constructor(passReceivePairs) {
+    if (!this._validatePassReceivePairs(passReceivePairs))
+      throw new Error("must pass in tuples of function names like [['handleData', 'onData'], ...]")
+
+    this._createState()
+
+    passReceivePairs.forEach(this._processPassReceivePair.bind(this))
+  }
+
+  // Remove all state. Useful for applications making heavy repeated use
+  // of single promises.
+  clear() {
+    this._resetState()
+  }
+
+  _createState() {
+    this._state = {}
+  }
+
+  _resetState() {
+    Object.keys(this._state).forEach(stateKey => {
+      this._state[stateKey] = []
+    })
+  }
+
+  _processPassReceivePair(pair) {
+    const [ pass, receive ] = pair
+
+    const stateKey = '__state_' + pass
+    const handlerKey = '__handler_' + receive
+
+    // This will contain the data from every pass call
+    this._state[stateKey] = []
+
+    this[receive] = (handler) => {
+      // set local handler for pass funk to call
+      this[handlerKey] = handler
+
+      // Get all data previously called from pass funk
+      this._state[stateKey].forEach(handler)
+    }
+
+    this[pass] = (data) => {
+      // Save data for future receive assignments to get
+      this._state[stateKey].push(data)
+
+      // Call receive function with data
+      this[handlerKey] && this[handlerKey](data)
+    }
+
+  }
+
+  _validatePassReceivePairs(passReceivePairs) {
+    const isArray = Array.isArray(passReceivePairs)
+    if (!isArray) return false
+
+    const hasSomeEntries = passReceivePairs.length >= 1
+    if (!hasSomeEntries) return false
+
+    const isTuples = passReceivePairs.every(pair => {
+      return pair.length === 2
+    })
+    if (!isTuples) return false
+
+    const areStrings = passReceivePairs.every(pair => {
+      return typeof pair[0] === 'string' &&
+             typeof pair[1] === 'string'
+    })
+    if (!areStrings) return false
+
+    return true
+  }
+}
+
 class Quic {
-  listen() {}
-  stopListening() {}
-  getServer() {}
+  constructor() {
+    this._server = new Server()
+  }
+
+  listen(port, address = 'localhost') {
+    if (!port) throw new Error('must supply port argument')
+
+    const promise = new CustomPromise([['resolve', 'then'], ['reject', 'onError'], ['handleData', 'onData']])
+
+    this._server
+      .on('error', (err) => rejectPromise(promise, err, 'server error'))
+      .on('session', (session) => {
+
+        session
+          .on('error', (err) => rejectPromise(promise, err, 'server session error'))
+          .on('stream', (stream) => {
+
+            let message = '' // TODO turn into buf?
+
+            stream
+              .on('error', (err) => rejectPromise(promise, err, 'server stream error'))
+              .on('data', (data) => {
+                message += data.toString()
+              })
+              .on('end', () => {
+                promise.resolve(message, stream)
+                stream.end()
+              })
+              .on('finish', () => {
+                // ilog.info(`server stream ${stream.id} finished`)
+              })
+          })
+      })
+
+    // this._server.listen(port, address).then(promise.resolve)
+    promise.resolve()
+
+    return promise
+  }
+
+  stopListening() {
+    this._server.close()
+  }
+
+  getServer() {
+    return this._server
+  }
+
   getClient() {}
-  getAddress() {}
+
+  getAddress() {
+    return this._server.address()
+  }
+
   send() {}
 }
 
-module.exports = new Quic()
+module.exports = { quic: new Quic(), CustomPromise }
--- test/module.spec.js
+++ test/module.spec.js
@@ -1,11 +1,53 @@
 /* eslint-env mocha */
-import { expect } from 'chai';
-import quic from '../src/index';
+import { expect } from 'chai'
+let quic = require('../src/index').quic
 
-describe('node-quic', () => {
+const CustomPromise = require('../src/index').CustomPromise
+
+describe.skip('node-quic', () => {
+
+  afterEach(() => {
+    // everything that results in a totally fresh quic server here
+    quic.stopListening()
+    quic = require('../src/index').quic
+  })
 
   it('is properly exported', () => {
     expect(quic).to.be.an('object')
+  })
+
+  describe('.getServer()', () => {
+
+    it('exists as a property', () => {
+      expect(quic.getServer).to.be.a('function')
+    })
+
+    it('returns server object', () => {
+      const server = quic.getServer()
+      expect(server).to.be.an('object')
+      expect(server).to.have.property('domain')
+      expect(server).to.have.property('localPort')
+      expect(server).to.have.property('localAddress')
+      expect(server).to.have.property('localFamily')
+      expect(server).to.have.property('listening')
+    })
+
+  })
+
+  describe('.getAddress()', () => {
+
+    it('exists as a property', () => {
+      expect(quic.getAddress).to.be.a('function')
+    })
+
+    it('returns address object', () => {
+      console.log(quic.getAddress())
+      const address = quic.getAddress()
+      expect(address).to.have.property('port')
+      expect(address).to.have.property('family')
+      expect(address).to.have.property('address')
+    })
+
   })
 
   describe('.listen()', () => {
@@ -14,13 +56,26 @@ describe('node-quic', () => {
       expect(quic.listen).to.be.a('function')
     })
 
+    it('requires port argument', () => {
+      expect(quic.listen.bind(quic, null, null)).to.throw()
+    })
+
+    it.skip('defaults ip to localhost', (done) => {
+      quic.listen(1234).then(() => {
+        expect(quic.getAddress()).to.eq('localhost')
+        done()
+      })
+    })
+
+    it.skip('takes and assigns ip argument', () => {
+      const address = '127.0.0.1'
+
+      quic.listen(1234, address).then(() => {
+        expect(quic.getAddress()).to.eq(address)
+      })
+    })
+
     it('returns promise', () => {});
-
-    it('requires port argument', () => {})
-
-    it('defaults ip to localhost', () => {})
-
-    it('takes and assigns ip argument', () => {})
 
     describe('returned promise', () => {
 
@@ -72,18 +127,6 @@ describe('node-quic', () => {
     })
   })
 
-  describe('.getServer()', () => {
-
-    it('exists as a property', () => {
-      expect(quic.getServer).to.be.a('function')
-    })
-
-    it('returns server object', () => {
-
-    })
-
-  })
-
   describe('.getClient()', () => {
 
     it('exists as a property', () => {
@@ -91,16 +134,6 @@ describe('node-quic', () => {
     })
 
     it('returns client object', () => {})
-
-  })
-
-  describe('.getAddress()', () => {
-
-    it('exists as a property', () => {
-      expect(quic.getAddress).to.be.a('function')
-    })
-
-    it('returns address object', () => {})
 
   })
 
@@ -136,3 +169,111 @@ describe('node-quic', () => {
   })
 
 })
+
+describe('CustomPromise', () => {
+  it('is properly exported', () => {
+    expect(CustomPromise).to.be.a('function')
+  })
+
+  it('throws with no input', () => {
+    expect(() => { new CustomPromise() }).to.throw()
+  })
+
+  it('throws with malformed input', () => {
+    expect(() => { new CustomPromise('bob') }).to.throw()
+    expect(() => { new CustomPromise(['bob']) }).to.throw()
+    expect(() => { new CustomPromise(['bob', 'jim']) }).to.throw()
+  })
+
+  describe('when given well formed input', () => {
+
+    const pass = 'pass'
+    const receive = 'receive'
+    const passReceivePairs = [[pass, receive]]
+
+    let customPromise
+
+    beforeEach(() => {
+      customPromise = new CustomPromise(passReceivePairs)
+    })
+
+    it('has clear attribute', () => {
+      expect(customPromise).to.have.property('clear')
+    })
+
+    it('assigns pass/receive methods to prototype', () => {
+      expect(customPromise).to.have.property(pass)
+      expect(customPromise).to.have.property(receive)
+    })
+
+    describe('when a receive function is given', () => {
+
+      let receivedData = null
+
+      beforeEach(() => {
+        customPromise[receive](data => { receivedData = data })
+      })
+
+      afterEach(() => {
+        receivedData = null
+      })
+
+      it('calls it with passed data', () => {
+        const data = 'jim'
+
+        customPromise[pass](data)
+        expect(receivedData).to.eq(data)
+      })
+    })
+
+    describe('before a receive function is given', () => {
+
+      it('allows pass function to be called', () => {
+        const data = 'fred'
+
+        expect(customPromise[pass].bind(null, data)).to.not.throw()
+      })
+    })
+
+    describe('when a pass function is called before a receive function', () => {
+
+      const data = 'barney'
+      let receivedData1 = null
+      let receivedData2 = null
+
+      beforeEach(() => {
+        customPromise[pass](data)
+        customPromise[receive](data => receivedData1 = data)
+        customPromise[receive](data => receivedData2 = data)
+      })
+
+      afterEach(() => {
+        receivedData1 = null
+        receivedData2 = null
+      })
+
+      it('calls receive function with previously passed data', () => {
+        expect(receivedData1).to.eq(data)
+      })
+
+      it('calls a second receive function with previously passed data', () => {
+        expect(receivedData2).to.eq(data)
+      })
+
+      describe('and then clear is called', () => {
+
+        let receivedData = null
+
+        beforeEach(() => {
+          customPromise.clear()
+          customPromise[receive](data => receivedData = data)
+        })
+
+        it('no longer populates new calls with old data', () => {
+          expect(receivedData).to.eq(null)
+        })
+      })
+    })
+  })
+})
+
