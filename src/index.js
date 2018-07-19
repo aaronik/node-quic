d54aecee99ae19e650b483078bfcaaa2c49dccb5
Aaron Sullivan <aasullivan1618@gmail.com>
2018-07-18 21:19:32 -0700

    Get rid of custom promise stuff, it's externalized

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { Client, Server } from 'quic'
+import ArbitraryPromise from 'arbitrary-promise'
 
 // quic.listen(2345) // also takes address, defaults to localhost
 //   .then(() => {})
@@ -22,84 +23,6 @@ const rejectPromise = (promise, err, message) => {
   promise.reject(Object.assign(err, { class: message }))
 }
 
-// A special promise with .onError and .onData functions
-// Also does not require executor function, and can have reject
-// called on its prototype
-class CustomPromise {
-  constructor(passReceivePairs) {
-    if (!this._validatePassReceivePairs(passReceivePairs))
-      throw new Error("must pass in tuples of function names like [['handleData', 'onData'], ...]")
-
-    this._createState()
-
-    passReceivePairs.forEach(this._processPassReceivePair.bind(this))
-  }
-
-  // Remove all state. Useful for applications making heavy repeated use
-  // of single promises.
-  clear() {
-    this._resetState()
-  }
-
-  _createState() {
-    this._state = {}
-  }
-
-  _resetState() {
-    Object.keys(this._state).forEach(stateKey => {
-      this._state[stateKey] = []
-    })
-  }
-
-  _processPassReceivePair(pair) {
-    const [ pass, receive ] = pair
-
-    const stateKey = '__state_' + pass
-    const handlerKey = '__handler_' + receive
-
-    // This will contain the data from every pass call
-    this._state[stateKey] = []
-
-    this[receive] = (handler) => {
-      // set local handler for pass funk to call
-      this[handlerKey] = handler
-
-      // Get all data previously called from pass funk
-      this._state[stateKey].forEach(handler)
-    }
-
-    this[pass] = (data) => {
-      // Save data for future receive assignments to get
-      this._state[stateKey].push(data)
-
-      // Call receive function with data
-      this[handlerKey] && this[handlerKey](data)
-    }
-
-  }
-
-  _validatePassReceivePairs(passReceivePairs) {
-    const isArray = Array.isArray(passReceivePairs)
-    if (!isArray) return false
-
-    const hasSomeEntries = passReceivePairs.length >= 1
-    if (!hasSomeEntries) return false
-
-    const isTuples = passReceivePairs.every(pair => {
-      return pair.length === 2
-    })
-    if (!isTuples) return false
-
-    const areStrings = passReceivePairs.every(pair => {
-      return typeof pair[0] === 'string' &&
-             typeof pair[1] === 'string'
-    })
-    if (!areStrings) return false
-
-    return true
-  }
-}
-
 class Quic {
   constructor() {
     this._server = new Server()
@@ -108,7 +31,7 @@ class Quic {
   listen(port, address = 'localhost') {
     if (!port) throw new Error('must supply port argument')
 
-    const promise = new CustomPromise([['resolve', 'then'], ['reject', 'onError'], ['handleData', 'onData']])
+    const promise = new ArbitraryPromise([['resolve', 'then'], ['reject', 'onError'], ['handleData', 'onData']])
 
     this._server
       .on('error', (err) => rejectPromise(promise, err, 'server error'))
@@ -158,4 +81,4 @@ class Quic {
   send() {}
 }
 
-module.exports = { quic: new Quic(), CustomPromise }
+module.exports = new Quic()
