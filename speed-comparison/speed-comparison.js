bf70c6186f5602691598e244a2c64c02175d6f7d
Aaron Sullivan <aasullivan1618@gmail.com>
2018-07-20 17:09:15 -0700

    Add potential for plurality in speed test

--- speed-comparison/speed-comparison.js
+++ speed-comparison/speed-comparison.js
@@ -8,18 +8,16 @@ import express from 'express'
 import request from 'request'
 import bodyParser from 'body-parser'
 
-const quicPort    = 8000
-const expressPort = 8001
 const address = '0.0.0.0'
 
-const runAsServer = () => {
+const runAsServer = (quicPort, httpPort) => {
 
   // The quic server
   quic.listen(quicPort, address)
     .onError(console.error)
     .then(() => console.log(`quic server listening at: ${address}:${quicPort}`))
     .onData((data, stream) => {
-      console.log('quic server bouncing:', data)
+      console.log(`quic server (${quicPort}) bouncing:`, data)
       stream.write(data)
     })
 
@@ -29,41 +27,52 @@ const runAsServer = () => {
 
   eApp.post('/test', (req, res) => {
     const data = req.body
-    console.log('express server bouncing:', data)
+    console.log(`express server (${httpPort}) bouncing:`, data)
     return res.json(data)
   })
 
   eApp.listen(
-    expressPort,
-    () => console.log(`express server listening at: ${address}:${expressPort}`)
+    httpPort,
+    () => console.log(`express server listening at: ${address}:${httpPort}`)
   )
 }
 
-const runAsClient = () => {
-  const data = { information: 'being posted!' }
+const runAsClient = (quicPort, httpPort) => {
+  const data = {
+    information: 'being posted!'
+  }
 
-  console.time('quic')
+  const quicTimeLabel = 'quic ' + quicPort
+  const httpTimeLabel = 'http ' + httpPort
+
+  console.time(quicTimeLabel)
   quic.send(quicPort, address, data)
     .onError(console.error)
     .onData(data => {
-      console.timeEnd('quic')
+      console.timeEnd(quicTimeLabel)
       console.log('quic client received data:', data)
     })
 
-  console.time('http')
+  console.time(httpTimeLabel)
   request({
     method: 'POST',
-    uri: `http://${address}:${expressPort}/test`,
+    uri: `http://${address}:${httpPort}/test`,
     body: data,
     json: true
   }, (err, resp) => {
     const data = resp.body
-    console.timeEnd('http')
+    console.timeEnd(httpTimeLabel)
     console.log('http client received data:', data)
   })
 }
 
 const role = process.argv[2]
 
-if (role === 'client') runAsClient()
-else                   runAsServer()
+const numSpinups = 1
+const startPort = 8000
+
+for (let p = 8000; p < startPort + numSpinups; p++) {
+  if (role === 'client') runAsClient(p, p + numSpinups)
+  else                   runAsServer(p, p + numSpinups)
+}
+
